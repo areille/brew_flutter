04bb58a0dd12e2c9fa45b5c3f9c7039bf3f2ac47
areille <areille@tuta.io>
2022-05-16 19:12:28 +0200

    :recycle: use riverpod for selected tab index

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,25 +1,22 @@
 import 'package:brew_flutter/home/tabs.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:macos_ui/macos_ui.dart';
 
-class Home extends StatefulWidget {
+final _selectedTabProvider = StateProvider((ref) => 0);
+
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int selectedTab = 0;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedTab = ref.watch(_selectedTabProvider);
     return MacosWindow(
       sidebar: Sidebar(
         minWidth: 200,
         builder: (_, controller) => SidebarItems(
           currentIndex: selectedTab,
-          onChanged: (i) => setState(() => selectedTab = i),
+          onChanged: (i) => ref.read(_selectedTabProvider.notifier).state = i,
           scrollController: controller,
           items: [
             for (final tab in Tabs.values)
